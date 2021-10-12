cba438252354bc90ee778d16e0c938600b9e014c
Michał Gawroński <79256098+GMichalCode@users.noreply.github.com>
2021-10-12 12:51:59 +0200

    added custom attribute to add to cart button in index.html

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,30 +1,31 @@
 addProductToCart();
 
 function addProductToCart() {
-    let buttons = document.getElementsByClassName("btn-success");
-    for (let button of buttons) {
-        button.addEventListener("click", async function () {
-            let productId = button.getAttribute("product-id")
-            await addProductToCartRequest({productId: productId});
-        })
-    }
+  let buttons = document.getElementsByClassName("btn-success");
+  for (let button of buttons) {
+    button.addEventListener("click", async function () {
+      console.debug("listener works");
+      let productId = button.getAttribute("product-id");
+      console.debug("get attribute (product id) works: " + productId);
+      // await addProductToCartRequest({productId: productId});
+    })
+  }
 }
 
-async function addProductToCartRequest(productId){
-     // todo add/cart
-    return await apiPost('/cart', productId);
-}
-
-async function apiPost(url, payload) {
-    let response = await fetch(url, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: payload
-    });
-    if (response.status === 200){
-        // ? .json() ?
-        return response;
-    }
-}
+// async function addProductToCartRequest(productId) {
+//   return await apiPost('/cart', productId);
+// }
+//
+// async function apiPost(url, payload) {
+//   let response = await fetch(url, {
+//     method: "POST",
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: payload
+//   });
+//   if (response.status === 200) {
+//     // ? .json() ?
+//     return response;
+//   }
+// }
